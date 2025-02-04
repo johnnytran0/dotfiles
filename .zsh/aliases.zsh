6c3257080bb4379084580e305e90d98c2790c6d8
Johnny Tran <48148643+johnnytran0@users.noreply.github.com>
2025-02-04 11:28:11 -0800

    add aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,7 +1,6 @@
 # Awesome platform-independent ls formatting
 # Adapted from code found at <http://www.reddit.com/r/linux/comments/hejra/til_nifty_ls_option_for_displaying_directories/c1utfxb>
 GLS_ARGS="--classify --tabsize=0 --literal --color=auto --show-control-chars --human-readable --group-directories-first"
-# alias ls="ls $GLS_ARGS"
 alias ls="gls $GLS_ARGS"
 # ls &> /dev/null
 # if [ $? -eq  ]; then # The environment ls isn't GNU ls; we're not on Linux
@@ -44,9 +43,10 @@ alias C='c; tmux clear-history; c;'
 alias canary='/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 alias chromed='chrome --auto-open-devtools-for-tabs'
+alias chromeh='chrome --headless'
 alias chromei='chrome --incognito'
 alias cpu='top -o cpu'
-alias dc=docker-compose
+alias dc='docker compose'
 alias docker_ip="docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"
 alias docker_host="echo $(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)"
 alias g=gcloud
@@ -55,11 +55,16 @@ alias json='python -m json.tool'
 alias jsonv='jq empty; echo $!'
 alias k=kubectl
 alias lsd='ls -lah | grep "^d"'
+alias oll='ollama'
 # https://github.com/tmuxinator/tmuxinator#shorthand
 alias mux=tmuxinator
-alias plantuml='plantuml -X'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy | pbpaste"
 alias srcz="source ~/.zshrc"
+
+# Microsoft 365
+alias msexcel='open -b com.microsoft.Excel'
+alias msppt='open -b com.microsoft.Powerpoint'
+alias msword='open -b com.microsoft.Word'
 
 zle -N clear-buffer C
 bindkey 'C-k' clear-buffer
